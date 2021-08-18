22e705ae54178fdc0a8ccb047b9e4dc80c899755
wassim <b.wassim1993@hotmail.fr>
2021-08-18 20:03:13 +0100

    init project

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by wassim on 8/18/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
